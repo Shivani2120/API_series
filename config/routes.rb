4cdc22c55127e7d83c0ddcd23ac33e80e3a9c3ef
Shivani Shilpi <shivanishilpi2120@gmail.com>
2023-02-21 00:11:22 +0530

    API series

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # devise_for :users
+  namespace :v1 do
+     resources :contacts
+     resources :sessions, only: [:create, :destroy]
+  end
+  resources :books
+end
